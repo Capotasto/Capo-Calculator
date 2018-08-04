1cb86bc2af6ef9262f4332b881472d20c44e8958
Capotasto <rocker519804@gmail.com>
2018-08-03 18:15:37 -0700

    temp

--- app/src/main/kotlin/com/funckyhacker/capocalc/MainActivity.kt
+++ app/src/main/kotlin/com/funckyhacker/capocalc/MainActivity.kt
@@ -8,11 +8,12 @@ import com.funckyhacker.capocalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private val binding : ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         findViewById<View>(android.R.id.content).systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
